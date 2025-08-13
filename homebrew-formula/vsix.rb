957ff4dbc0732bb93ffde3e9e4c4a64431781d36
GitHub Action <action@github.com>
2025-08-13 19:43:39 +0000

    Update Homebrew formula for v1.0.0

--- homebrew-formula/vsix.rb
+++ homebrew-formula/vsix.rb
@@ -7,20 +7,20 @@ class Vsix < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/beeltec/vsix/releases/download/v1.0.0/vsix-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_ARM64"
+      sha256 "69088c67cdf48c49772b85d31032cf7d784aa293d6f0231e63eb83e33776c0ba"
     else
       url "https://github.com/beeltec/vsix/releases/download/v1.0.0/vsix-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_X86_64"
+      sha256 "5a0620836a6d86b5e362c0db5a90b6d9e923409ec8ce45e81a377ac1b08d3cf4"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/beeltec/vsix/releases/download/v1.0.0/vsix-aarch64-linux.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_ARM"
+      sha256 "9bcef7247839c54d5e547b2483503b1e5ef52f9989c6d930eb8dd54f45a7a292_ARM"
     else
       url "https://github.com/beeltec/vsix/releases/download/v1.0.0/vsix-x86_64-linux.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_X86"
+      sha256 "9bcef7247839c54d5e547b2483503b1e5ef52f9989c6d930eb8dd54f45a7a292_X86"
     end
   end
 
