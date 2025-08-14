3198e6cf69309d9e0e9361a6e67ead82743fef94
Christian Beelte <cb@beelte.com>
2025-08-14 07:30:24 +0200

    chore(domain): export installation strategy module

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -1,11 +1,13 @@
 pub mod entities;
 pub mod errors;
+pub mod installation_strategy;
 pub mod repositories;
 pub mod sort;
 pub mod value_objects;
 
 pub use entities::*;
 pub use errors::*;
+pub use installation_strategy::*;
 pub use repositories::*;
 pub use sort::*;
 pub use value_objects::*;
