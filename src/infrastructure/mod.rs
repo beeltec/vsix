68a5e288ea89188ce1ccb423b2d12c06b5081128
Christian Beelte <cb@beelte.com>
2025-08-14 07:30:46 +0200

    chore(infrastructure): export installation service module

--- src/infrastructure/mod.rs
+++ src/infrastructure/mod.rs
@@ -1,7 +1,9 @@
 pub mod file_system;
+pub mod installation_service;
 pub mod marketplace_client;
 #[cfg(test)]
 mod marketplace_tests;
 
 pub use file_system::*;
+pub use installation_service::*;
 pub use marketplace_client::*;
